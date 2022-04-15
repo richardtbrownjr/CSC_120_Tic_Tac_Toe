372da6801dbe5d745848a8300fc4b4c43f78f861
Rick <richardtbrownjr@gmail.com>
2022-04-15 16:15:09 -0400

    added user

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CSC_120_Tic_Tac_Toe
+# add the hardest time getting the password keygen stuff to work.
--- board.py
+++ board.py
@@ -1,11 +1,46 @@
 # CSC_120_Tic_Tac_Toe
 #stage 1
 
-print("Printing Board")
-row1 = ["['-','-','-',]"]
-row2 = ["['-','-','-',]"]
-row3 = ["['-','-','-',]"]
-board = [row1,row2,row3]
-print(*board, sep='\n')
-print("board printed")
+board = ["-","-","-","-","-","-","-","-","-",]
+user = True
 #https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways/
+
+def printboard(board):
+    print("Printing Board")
+    for i in range(0, len(board), 3):
+            print(*board[i:i+3], sep=' ')
+            print()
+        #for i in range(0, len(list), 5):
+    #print(*list[i:i+5], sep='')
+    print("board printed")
+
+def checkboard(userinput, board, playerup):
+    if board[userinput] == "-":
+        board.insert(userinput, playerup)
+        printboard(board)
+    else:
+        print("position already taken")
+
+def quit(userinput):
+    if userinput == "q":
+        return True
+    else:
+        return False
+def whoison(user):
+    if user:
+        return "x"
+    else:
+        return "o"
+
+while True:
+    playerup = whoison(user)
+    userinput = int(input("Please enter 1-9 or 'q' to quit: "))
+    if quit(userinput):
+        break
+    if userinput in range(1,9):
+        checkboard(userinput, board, playerup)
+    user = not user
+
+#https://docs.gitlab.com/ee/user/project/repository/gpg_signed_commits/#associate-your-gpg-key-with-git
+
+printboard(board)
