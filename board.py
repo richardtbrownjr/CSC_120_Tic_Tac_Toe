914b549eb58ffdd997bf611fa86fbf70bcce12bf
Rick <richardtbrownjr@gmail.com>
2022-04-21 12:25:15 -0400

    added when player wins

--- board.py
+++ board.py
@@ -2,6 +2,7 @@
 #stage 1
 import os
 
+#board = ["-","-","-","-","-","-","-","-"]
 board = ["-","-","-","-","-","-","-","-"]
 user = True
 #https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways/
@@ -9,12 +10,12 @@ user = True
 def printboard(board):
     os.system('clear')
     print("Printing Board")
+    #print(board)
     for i in range(0, len(board), 3):
-            print(*board[i:i+3], sep=' ')
-            print()
-        #for i in range(0, len(list), 5):
-    #print(*list[i:i+5], sep='')
+        print(*board[i:i+3], sep=' ')
+        print()
     print("board printed")
+    whowon(board,playerup)
 
 def checkboard(userinput, board, playerup):
     if board[userinput] == "-":
@@ -24,7 +25,36 @@ def checkboard(userinput, board, playerup):
     else:
         print("position already taken")
 
-def quit(userinput):
+def whowon(board,playerup):
+    #across wins
+    if (board[0] and board[1] and board[2]) == playerup:
+        print("Player", playerup, "won")
+        #quit()
+    if (board[3] and board[4] and board[5]) == playerup:
+        print("Player", playerup, "won")
+        #quit()
+    if (board[6] and board[7] and board[8]) == playerup:
+        print("Player", playerup, "won")
+        #quit()
+    #up and down wins
+    if (board[0] and board[3] and board[6]) == playerup:
+        print("Player", playerup, "won")
+        #quit()
+    if (board[1] and board[4] and board[7]) == playerup:
+        print("Player", playerup, "won")
+        #quit()
+    if (board[2] and board[5] and board[8]) == playerup:
+        print("Player", playerup, "won")
+        #quit()
+    #diagonal wins
+    if (board[0] and board[4] and board[8]) == playerup:
+        print("Player", playerup, "won")
+        #quit()
+    if (board[2] and board[4] and board[6]) == playerup:
+        print("Player", playerup, "won")
+        #quit()
+
+def quit1(userinput):
     if userinput == 11:
         return True
     else:
@@ -38,7 +68,7 @@ def whoison(user):
 while True:
     playerup = whoison(user)
     userinput = int(input("Please enter 1-9 or 11 to quit: "))
-    if quit(userinput):
+    if quit1(userinput):
         break
     if userinput in range(1,9):
         checkboard(userinput, board, playerup)
